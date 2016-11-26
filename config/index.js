880c9b1111633c84964217ed545af399e7b50024
Bill Camphire <308code@gmail.com>
2016-11-25 21:08:16 -0500

    Added angular and bootstrap functionality for homepage

--- config/index.js
+++ config/index.js
@@ -2,9 +2,6 @@ var configValues = require('./config');
 
 module.exports = {
     getDbConnectionString: function () {
-        console.log('mongodb://' + configValues.username + ':' +
-            configValues.password + 
-            '@ds161497.mlab.com:61497/' + configValues.database);
         return 'mongodb://' + configValues.username + ':' +
             configValues.password + 
             '@ds161497.mlab.com:61497/' + configValues.database;
--- public/app.js
+++ public/app.js
@@ -1,6 +1,17 @@
-var gmkShowMetricsApp = angular.module('gmkShowMetricsApp',[]);
+var gmkShowMetricsApp = angular.module('gmkShowMetricsApp', []);
 
-gmkShowMetricsApp.controller('homePageController',function($scope){
-    $scope.temp = "Lets See if this works!!";
-    console.log($scope);
+gmkShowMetricsApp.controller('homePageController', function ($scope, $log, $http) {
+
+    //var getAllShows = function () {
+        $http({ method: 'GET', url: '/api/shows', type: 'application/json' })
+            .then(function (response) {
+                $log.info(response.data);
+                $scope.shows = response.data;
+            }, function (response) {
+                $log.error(response);
+            }
+            );
+   //};
+
+    //$scope.shows = getAllShows();
 });
